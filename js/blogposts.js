650c43b4bb394a1fa74f7254bf42206865a1aa55
Benjamin Løndal <benjamin.londal@gmail.com>
2022-09-02 11:28:37 +0200

    api test

---
+++ js/blogposts.js
@@ -0,0 +1,40 @@
+const blogPostsUrl = "https://blog-api-posts.dvergnir.one/wp-json/wp/v2/posts?_embed";
+const blogContainer = document.querySelector(".blog-content");
+
+
+
+async function getBlogs(blogPostsUrl){
+
+
+    try {
+        const response = await fetch(blogPostsUrl);
+        const blogPosts = await response.json();
+
+
+        blogPosts.forEach(function(blogPost){
+
+            const blogImage = blogPost._embedded?.['wp:featuredmedia'][0]['source_url'];
+            const placeholder = "https://via.placeholder.com/150";
+
+            if (blogImage ==! Image) {
+                return placeholder;
+            } else { return blogImage };
+
+
+            blogContainer.innerHTML += `<div class="blog-container">
+                                            <p>${blogPost.date}</p>
+                                            <h2>${blogPost.title.rendered}</h2>
+                                            <img  src="${blogImage}"class="blogposts-image">
+                                            <p>${blogPost.excerpt.rendered}</p>
+                                        </div>`;
+
+        });
+    } catch(error) {
+        console.log(error);
+    }
+}
+   
+
+getBlogs(blogPostsUrl);
+
+
