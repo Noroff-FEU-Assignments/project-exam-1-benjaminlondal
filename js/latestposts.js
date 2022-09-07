2aa05162a722178e6c09a0ad69550c503e1b87d9
Benjamin Løndal <benjamin.londal@gmail.com>
2022-09-07 14:33:58 +0200

    update css,js

---
+++ js/latestposts.js
@@ -0,0 +1,37 @@
+const blogPostsUrl = "https://blog-api-posts.dvergnir.one/wp-json/wp/v2/posts?_embed";
+const postContainer = document.querySelector(".latest-container");
+
+
+
+async function getBlogs(blogPostsUrl){
+
+
+    try {
+        const response = await fetch(blogPostsUrl);
+        const blogPosts = await response.json();
+
+
+        for(var i = 0; i < blogPosts.length; i++) {
+
+            if (i === 3) {
+                break;
+            }
+
+            const blogImage = blogPosts[i]._embedded["wp:featuredmedia"]?.[0]["source_url"] ?? "https://via.placeholder.com/150";
+
+            postContainer.innerHTML += `<div class="blog-container latest-posts">
+                                            <img  src="${blogImage}"class="blogposts-image">
+                                            <h3>${blogPosts[i].title.rendered}</h3>
+                                            <p>${blogPosts[i].excerpt.rendered}</p>
+                                            <a href="blog-specific.html?id=${blogPosts[i].id}" class="post-link latest-link">Read more</a>
+                                        </div>`;
+
+        }
+
+    } catch(error) {
+        console.log(error);
+    }
+}
+   
+
+getBlogs(blogPostsUrl);
