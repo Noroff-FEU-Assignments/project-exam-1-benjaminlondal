3c76e66b1a56f771ae63eb73d8031d1a69cd7fe2
Benjamin Løndal <benjamin.londal@gmail.com>
2022-09-15 16:49:21 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,25 +1,3 @@
 import { displayNavigation } from "./components/functions.js";
 
 displayNavigation();
-
-setTimeout(() => {
-    const modal = document.getElementById("modal-open");
-    const showModal = document.getElementById("modal-closed");
-
-showModal.addEventListener("click", () => {
-    modal.style.display = "block";
-});
-
-}, "1000");
-
-
-
-
-window.addEventListener("click", function (event) {
-    const modal = document.getElementById("modal-open");
-
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-});
-
